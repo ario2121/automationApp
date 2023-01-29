35245b01a5a861269d68206d5b041ee917f2b96c
ario2121 <hosein.iup93@gmail.com>
2023-01-29 23:01:25 +0330

    add main file to repositories

---
+++ main.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+# driver = webdriver.Chrome(executable_path="C:\chromedriver.exe")
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+driver.get("https://www.google.de/")
